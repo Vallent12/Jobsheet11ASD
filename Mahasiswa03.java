3250d7764cf156ea98c490b8801c644bc8b21fce
Vallent12 <fevadtya09.gmail.com>
2026-05-08 20:35:51 +0700

    Praktikum01

---
+++ Mahasiswa03.java
@@ -0,0 +1,17 @@
+public class Mahasiswa03 {
+    String nim; 
+    String nama; 
+    String kelas;
+    double ipk;
+
+    public Mahasiswa03(String nm, String name, String kls, double ip) {
+        this.nim = nm;
+        this.nama = name;
+        this.kelas = kls;
+        this.ipk = ip;
+    }
+
+    public void tampilInformasi() {
+        System.out.printf("%-10s %-12s %-5s %.1f%n", nama, nim, kelas, ipk);
+    }
+}
